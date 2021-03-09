e9204fb6ab95937ef354f76307537b3dc4a47a75
piercenovoa <piercenh17@gmail.com>
2021-03-09 18:00:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css"
+
+const App = () => {
+
+    return (
+        <div className="app">
+            <h1>Hi wiiiii</h1>
+        </div>
+    )
+}
+
+
+export default App;
